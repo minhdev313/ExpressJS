3094c7da30dbe32c2ce5180db701eb1608895daf
minhdev313 <103765171+minhdev313@users.noreply.github.com>
2024-01-07 19:50:22 +0700

    7-Jan

---
+++ EX2/index.js
@@ -0,0 +1,24 @@
+const rectangle = require('./rectangle.js');
+var rect = 
+    require('./rectangle.js');
+
+function solveRect(l, b){
+    console.log("Solving for rectangle with l = " + l + " and b = "  + b);
+    rect(l,b, (err, rectangle) => {
+        if (err){
+            console.log("ERROR: ", err.message);
+        }
+        else{
+            console.log("The area of the rectangle is " + rectangle.area());
+            console.log("The perimeter of the rectangle is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect(");
+
+};
+
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(-3, 5);
+
